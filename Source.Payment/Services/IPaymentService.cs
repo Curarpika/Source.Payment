a46dcf859ae1d567d1598a0f3c4fbc2ae272c8e3
Eddy <85140452@qq.com>
2019-05-23 13:09:02 +0800

    changed

--- Source.Payment/Services/IPaymentService.cs
+++ Source.Payment/Services/IPaymentService.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using Source.Payment.Midels.AccountViewModels;
-using Source.Payment.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
--- Source.WebAPI/Controllers/HomeController.cs
+++ Source.WebAPI/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Source.Payment;
 
 namespace Source.WebAPI
 {

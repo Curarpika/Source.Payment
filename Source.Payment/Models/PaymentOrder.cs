e036edd2e1d9b545474857e3ab65ef8c5b372145
Eddy <85140452@qq.com>
2019-05-31 17:59:54 +0800

    mqtt update

--- Source.Payment/Models/PaymentOrder.cs
+++ Source.Payment/Models/PaymentOrder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using Newtonsoft.Json;
 using Source.Payment.Bases;
@@ -14,12 +15,21 @@ namespace Source.Payment
         public string UserId { get; set; }
         public decimal Amount { get; set; }
         public int Quantity { get; set; }
-        public OrderType OrderType { get; set;}
-        public PayMethod PayMethod { get; set;}
-        public OrderState OrderState { get; set;}
+        public OrderType OrderType { get; set; }
+        public PayMethod PayMethod { get; set; }
+        public OrderState OrderState { get; set; }
         // 回调订单ID
-        public String PaymentOrderId{ get; set; }
+        public String PaymentOrderId { get; set; }
         [JsonConverter(typeof(ShortDateConverter))]
         public DateTime? PaidTime { get; set; }
+        [NotMapped]
+        public string Code
+        {
+            get
+            {
+                return GuidEncoder.Encode(Id).Substring(0, 4).ToUpper();
+            }
+        }
     }
+
 }

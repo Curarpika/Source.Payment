d4e7f92300ba028513979f0ca675c3fd26bd9f82
Curarpika <Curarpika@users.noreply.github.com>
2019-06-01 14:43:38 +0800

    update

--- Source.WebAPI/Controllers/WxPayController.cs
+++ Source.WebAPI/Controllers/WxPayController.cs
@@ -156,7 +156,7 @@ namespace Source.WebAPI.Controllers
                 ViewData["nonceStr"] = nonceStr;
                 ViewData["package"] = package;
                 ViewData["paySign"] = TenPayV3.GetJsPaySign(TenPayV3Info.AppId, timeStamp, nonceStr, package, TenPayV3Info.Key);
-                ViewData["successUrl"] = Url.Action("WxPayOrder", "Home");
+                ViewData["successUrl"] = Url.Action("WxPayOrder", "WxPay", new {orderid = orderid});
 
                 //临时记录订单信息，留给退款申请接口测试使用
                 HttpContext.Session.SetString("BillNo", sp_billno);
